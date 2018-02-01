eed5e804c948316727d3e9563b38e89f519172be
Harlan1994 <1041099438@qq.com>
2018-02-01 12:29:32 +0800

    添加二维矩阵搜索算法

--- ninechapter/search_2d_matrix.cpp
+++ ninechapter/search_2d_matrix.cpp
@@ -7,16 +7,25 @@ using namespace std;
 
 /**
  *
+ * 问题描述：
+ * 在一个 m*n 整数矩阵中找到指定值 target。
+ * 这个整数矩阵有如下性质：
+ * 1. 每行从左到右数值递增
+ * 2. 每列从上到下数值递增
+ *
+ * 解决方案：
  * 这样的矩阵有一个特点就是：每一个元素下方的元素以及右方的元素总是比它大，反之比它小。
  * 那如果从左下角开始比较，比目标元素大的话，只需要搜上方的矩阵即可，而比目标元素小的话
  * 只需要搜右方的矩阵即可。
  *
- * temp == target return 1
+ * 具体做法：
+ * temp == target return true
  * temp < target find its right sub matrix
  * temp > target find its upper sub matrix
- * 
+ * return false if not found eventually.
+ *
  * @param matrix
- * @param target
+ * @param target to be found.
  * @return
  */
 bool search(int matrix[][5], int target, int col, int row) {
